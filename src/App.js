c8445be4844eebefbcd65ba909f9c31ffe6d9847
AllComein <144424426+AllComein@users.noreply.github.com>
2023-12-04 16:38:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       {/* <Excelf/> */}
-
+    Only Use .XLSX File to compare don't use any other File type
       <Compexcel/>
     </div>
   );
