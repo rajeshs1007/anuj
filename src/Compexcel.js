704fe4afdba3a120da9b08132c31cfe172ce692e
AllComein <144424426+AllComein@users.noreply.github.com>
2023-12-05 16:28:06 +0530

    Update Compexcel.js

--- src/Compexcel.js
+++ src/Compexcel.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 import readXlsxFile from "read-excel-file";
-import * as XLSX from "xlsx"; // Import XLSX library
-
+import * as XLSX from "xlsx";
 import { Container, Grid, Segment } from "semantic-ui-react";
 import { Table, Button, Icon, Message } from "semantic-ui-react";
 
@@ -11,17 +10,18 @@ const App = () => {
   const [File1, setFile1] = useState([]);
   const [File2, setFile2] = useState([]);
   const [Result, setResult] = useState([]);
-  const [ColumnIndex1, setColumnIndex1] = useState(1); // Default to compare the second column of File 1
-  const [ColumnIndex2, setColumnIndex2] = useState(18); // Default to compare the second column of File 1
-  const [ColumnIndex3, setColumnIndex3] = useState(0); // Default to compare the second column of File 2
-  const [ColumnIndex4, setColumnIndex4] = useState(4); // Default to compare the second column of File 2
-  const [ColumnIndex0File1, setColumnIndex0File1] = useState(0); // Default to compare the first column of File 1
-  const [ColumnIndex0File2, setColumnIndex0File2] = useState(1); // Default to compare the first column of File 2
+  const [ColumnIndex1, setColumnIndex1] = useState(1);
+  const [ColumnIndex2, setColumnIndex2] = useState(18);
+  const [ColumnIndex3, setColumnIndex3] = useState(0);
+  const [ColumnIndex4, setColumnIndex4] = useState(4);
+  const [ColumnIndex0File1, setColumnIndex0File1] = useState(0);
+  const [ColumnIndex0File2, setColumnIndex0File2] = useState(1);
   const [file1Uploaded, setFile1Uploaded] = useState(false);
   const [file2Uploaded, setFile2Uploaded] = useState(false);
   const [showDifferenceRows, setShowDifferenceRows] = useState(false);
-
   const [lastUploadedFile, setLastUploadedFile] = useState(null);
+  const [fileName1, setFileName1] = useState(null);
+  const [fileName2, setFileName2] = useState(null);
 
   const handleFile = (e) => {
     const name = e.target.files[0].name;
@@ -31,7 +31,7 @@ const App = () => {
 
     if (!e || !e.target || !e.target.files || e.target.files.length === 0) {
       alert("Invalid file. Please upload a valid file.");
-      window.location.reload(); // Refresh the page
+      window.location.reload();
       return;
     }
 
@@ -39,7 +39,7 @@ const App = () => {
       if (lastUploadedFile !== name) {
         alert("Invalid file type. Please upload a valid Excel file (xlsx).");
         setLastUploadedFile(name);
-        window.location.reload(); // Refresh the page
+        window.location.reload();
       }
       return;
     }
@@ -55,6 +55,7 @@ const App = () => {
         });
         setFile1(a);
         setFile1Uploaded(true);
+        setFileName1(name);
       });
     }
 
@@ -68,6 +69,7 @@ const App = () => {
         });
         setFile2(a);
         setFile2Uploaded(true);
+        setFileName2(name);
       });
     }
   };
@@ -269,11 +271,11 @@ if (extraRowsInFile2.length > 0) {
         <Table.Cell key={`extra-cell-0`} ><Icon name="arrow right" color="red" />N/A</Table.Cell>  {/*// File 2 Column 0 */}
          <Table.Cell key={`extra-cell-1`}><Icon name="arrow right" color="red" />N/A</Table.Cell> {/* // File 2 Column 1 */}
          <Table.Cell key={`extra-cell-2`}><Icon name="arrow right" color="red" />N/A</Table.Cell>  {/*// File 2 Column 2 */}
-         <Table.Cell key={`extra-cell-3`}><Icon name="arrow right" color="green" />{row[1]}</Table.Cell> {/* // File 1 Column 0 (show "N/A") */}
-         <Table.Cell key={`extra-cell-4`}><Icon name="arrow right" color="green" />{row[0]}</Table.Cell> {/* // File 1 Column 1 (show "N/A") */}
-         <Table.Cell key={`extra-cell-5`}><Icon name="arrow right" color="green" />{row[4]}</Table.Cell> {/* // File 1 Column 2 (show "N/A") */}
+         <Table.Cell key={`extra-cell-3`}><Icon name="arrow right" color="green" />{row[ColumnIndex0File2]}</Table.Cell> {/* // File 1 Column 0 (show "N/A") */}
+         <Table.Cell key={`extra-cell-4`}><Icon name="arrow right" color="green" />{row[ColumnIndex3]}</Table.Cell> {/* // File 1 Column 1 (show "N/A") */}
+         <Table.Cell key={`extra-cell-5`}><Icon name="arrow right" color="green" />{row[ColumnIndex4]}</Table.Cell> {/* // File 1 Column 2 (show "N/A") */}
          <Table.Cell key={`extra-cell-6`}>Extra Row</Table.Cell>
-         <Table.Cell key={`extra-cell-7`}>{row[4]}</Table.Cell>
+         <Table.Cell key={`extra-cell-7`}>{-1*row[ColumnIndex4]}</Table.Cell>
       </Table.Row>
     ));
 
@@ -291,40 +293,85 @@ if (extraRowsInFile2.length > 0) {
 
 
 
-const handleDownload = () => {
-  if (Result.length === 0) {
-    alert("Result is empty. Nothing to download.");
-    return;
-  }
-
-  const data = [
-    ["File 1 Column 0", "File 1 Column 1", "File 1 Column 2", "File 2 Column 0", "File 2 Column 1", "File 2 Column 2", "Isin Diff", "Q Diff"]
-  ];
-
-  Result.forEach((row) => {
-    const rowData = [];
-    for (let i = 0; i < 8; i++) {
-      const cellValue = row.props.children[i]?.props?.children;
-
-      // Check the data type of the cell value
-      if (typeof cellValue === "number") {
-        // Handle numeric values
-        rowData.push(cellValue);
-      } else {
-        // Handle non-numeric (e.g., alphanumeric) values
-        rowData.push(cellValue || ""); // Ensure empty string for null values
-      }
-    }
-    data.push(rowData);
-  });
-
-  const ws = XLSX.utils.aoa_to_sheet(data);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, "Comparison Result");
-  XLSX.writeFile(wb, "comparison_result.xlsx");
-};
-
-
+  // const handleDownload = () => {
+  //   if (Result.length === 0) {
+  //     alert("Result is empty. Nothing to download.");
+  //     return;
+  //   }
+  
+  //   const data = [
+  //     ["File 1 Column 0", "File 1 Column 1", "File 1 Column 2", "File 2 Column 0", "File 2 Column 1", "File 2 Column 2"],
+  //   ];
+  
+  //   Result.forEach((row) => {
+  //     const rowData = [];
+  //     for (let i = 0; i < 6; i++) {
+  //       const cellValue = row.props.children[i]?.props?.children;
+  
+  //       // Check the data type of the cell value
+  //       if (typeof cellValue === "number") {
+  //         // Handle numeric values
+  //         rowData.push(cellValue);
+  //       } else {
+  //         // Handle non-numeric (e.g., alphanumeric) values
+  //         rowData.push(cellValue || ""); // Ensure empty string for null values
+  //       }
+  //     }
+  //     data.push(rowData);
+  //   });
+  
+  //   // Add headers for stored values and compared values
+  //   data.push([
+  //     "Stored Value 1",
+  //     "Stored Value 2",
+  //     "Compared Value",
+  //     "Formula for Stored Value 1",
+  //     "Formula for Stored Value 2",
+  //     "Formula for Compared Value",
+  //   ]);
+  
+  //   // Iterate over the original data files (File1 and File2)
+  //   for (let i = 0; i < File1.length; i++) {
+  //     const value1Col1 = File1[i][ColumnIndex1];
+  //     const value1Col2 = File1[i][ColumnIndex2];
+  
+  //     let rowInFile2 = -1;
+  //     for (let j = 0; j < File2.length; j++) {
+  //       const value2Col1 = File2[j][ColumnIndex3];
+  
+  //       if (value1Col1 === value2Col1) {
+  //         rowInFile2 = j;
+  //         break;
+  //       }
+  //     }
+  
+  //     let comparedValue = 0;
+  //     if (rowInFile2 !== -1) {
+  //       comparedValue = value1Col2 - File2[rowInFile2][ColumnIndex4];
+  //     }
+  
+  //     // Determine whether to display values or formulas
+  //     const showValues = true; // Change this value to false to display formulas
+  
+  //     data.push([
+  //       File1[i][ColumnIndex0File1],
+  //       File1[i][ColumnIndex1],
+  //       File1[i][ColumnIndex2],
+  //       showValues ? File1[i][ColumnIndex1] : `=INDEX(Result, ${i + 1}, ${ColumnIndex1 + 1})`,
+  //       showValues ? File1[i][ColumnIndex2] : `=INDEX(Result, ${i + 1}, ${ColumnIndex2 + 1})`,
+  //       showValues ? comparedValue : `=INDEX(Result, ${i + 1}, ${ColumnIndex2 + 1})-INDEX(File2, MATCH(INDEX(Result, ${i + 1}, ${ColumnIndex1 + 1}), File2, 0), ${ColumnIndex4 + 1})`,
+  //     ]);
+  //   }
+  
+  //   // Convert data to an XLSX-compatible format
+  //   const ws = XLSX.utils.aoa_to_sheet(data);
+  //   const wb = XLSX.utils.book_new();
+  //   XLSX.utils.book_append_sheet(wb, ws, "Comparison Result");
+  
+  //   // Write the Excel file
+  //   XLSX.writeFile(wb, "comparison_result.xlsx");
+  // };
+  
   
   
   
@@ -487,7 +534,7 @@ const handleDownload = () => {
       </Grid>
       {file1Uploaded ? (
         <Message positive>
-          <Message.Header>File 1 is uploaded!</Message.Header>
+          <Message.Header>{`File 1 (${fileName1}) is uploaded!`}</Message.Header>
         </Message>
       ) : (
         <Message negative>
@@ -496,7 +543,7 @@ const handleDownload = () => {
       )}
       {file2Uploaded ? (
         <Message positive>
-          <Message.Header>File 2 is uploaded!</Message.Header>
+          <Message.Header>{`File 2 (${fileName2}) is uploaded!`}</Message.Header>
         </Message>
       ) : (
         <Message negative>
@@ -517,12 +564,12 @@ const handleDownload = () => {
       <Table celled>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>File 1 Column {ColumnIndex0File1}</Table.HeaderCell>
-            <Table.HeaderCell>File 1 Column {ColumnIndex1}</Table.HeaderCell>
-            <Table.HeaderCell>File 1 Column {ColumnIndex2}</Table.HeaderCell>
-            <Table.HeaderCell>File 2 Column {ColumnIndex0File2}</Table.HeaderCell>
-            <Table.HeaderCell>File 2 Column {ColumnIndex3}</Table.HeaderCell>
-            <Table.HeaderCell>File 2 Column {ColumnIndex4}</Table.HeaderCell>
+            <Table.HeaderCell>PORTFOLIO Column {ColumnIndex0File1}</Table.HeaderCell>
+            <Table.HeaderCell>PORTFOLIO Column {ColumnIndex1}</Table.HeaderCell>
+            <Table.HeaderCell>PORTFOLIO Column {ColumnIndex2}</Table.HeaderCell>
+            <Table.HeaderCell>DPholding Column {ColumnIndex0File2}</Table.HeaderCell>
+            <Table.HeaderCell>DPholding Column {ColumnIndex3}</Table.HeaderCell>
+            <Table.HeaderCell>DPholding Column {ColumnIndex4}</Table.HeaderCell>
             <Table.HeaderCell>Isin Diff</Table.HeaderCell>
             <Table.HeaderCell>Q Diff</Table.HeaderCell>
           </Table.Row>
